9bde125e49531f9458965147f17d6360d72e4b9a
T Orr <timothyorr04@gmail.com>
2025-01-24 08:08:00 -0800

    Update on progress

--- main.py
+++ main.py
@@ -18,13 +18,28 @@ def finder(values, centroids):
         output.append(i)
         color.append(sum(picked))
         matches.append(picked)
-    return np.array(output), np.array(color), np.array(matches)
+    return np.array(output), np.array(color)
 
 def recentroid(values, centroids):
-
-
-
-
+    dict = {}
+    dictC = {}
+    for i in centroids:
+        dict[tuple(i)] = 0
+        dictC[tuple(i)] = 0
+    for i in values:
+        closest = 1000000000000
+        picked = [0,0]
+        for n in centroids:
+            check = ((i[0]-n[0])**2+(i[1]-n[1])**2)**0.5
+            if check < closest:
+                closest = check
+                picked = n
+        dict[tuple(picked)] += i
+        dictC[tuple(picked)] += 1
+    newCentroids = []
+    for i in dict:
+        newCentroids.append(dict[i] / dictC[i])
+    return np.array(newCentroids)
 
 def firstDraft(numClusters, values):
     centroids = []
@@ -47,17 +62,19 @@ def firstDraft(numClusters, values):
         numClusters -= 1
     return np.array(centroids)
 
-
-
 plt.figure(figsize=(7,7))
 (X, y) = make_blobs(n_samples=500, n_features=2, cluster_std=0.5, centers=10)
 plt.scatter(X[:,0], X[:,1], c=y)
 plt.show()
 
-
 centroids = firstDraft(10, X)
-(X, y, centroids) = finder(X, centroids)
-recentroid(X, centroids)
+(X, y) = finder(X, centroids)
+
+old_centroids = np.array([0,0])
+while not np.array_equal(old_centroids, centroids):
+    old_centroids = centroids
+    centroids = recentroid(X, centroids)
+    (X, y) = finder(X, centroids)
 
 fig = plt.figure(figsize=(8, 8))
 plt.scatter(x=X[:,0], y=X[:,1], c = y)
